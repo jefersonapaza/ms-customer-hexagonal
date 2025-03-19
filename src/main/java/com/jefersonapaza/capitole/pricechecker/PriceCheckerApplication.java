8985adf10d41326787f2230faa9dc10499097c73
Jeferson Apaza <jefersonapaza@gmail.com>
2025-03-19 12:39:54 -0500

    Initial commit: base Spring Boot project

---
+++ src/main/java/com/jefersonapaza/capitole/pricechecker/PriceCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.jefersonapaza.capitole.pricechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefersonapaza/capitole/pricechecker/PriceCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefersonapaza.capitole.pricechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
